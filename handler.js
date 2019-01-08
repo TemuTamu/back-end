7deac3b4f7bda1e5ed778ce83d63fb3aff4fa26f
pisangGoreng <7ofpentacles@gmail.com>
2019-01-08 10:31:42 +0700

    add jest

--- handler.js
+++ handler.js
@@ -1,14 +1,17 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
+const HelloWorld = require('./lib/hello-world');
+
+module.exports.helloWorld = (event, context, callback) => {
+  var hWorld = new HelloWorld();
+
+  const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    headers: {
+      'Access-Control-Allow-Origin': '*' // Required for CORS support to work
+    },
+    body: JSON.stringify(hWorld.sayHello(event))
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
